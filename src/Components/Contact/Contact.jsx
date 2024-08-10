75191049f0a37f5289fe60d4c038325eff3bf502
Leader <abdo.gammal1233@gmail.com>
2024-08-10 14:33:59 +0300

    last

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import style from './contact.module.css' /*هونيييييك */
 
 export default function Contact() {
+    const [isFocus, setIsFocus] = useState({
+        name: false,
+        age: false,
+        email: false,
+        pass: false,
+    })
+    function change (e) {
+        setIsFocus({...isFocus, [e.target.name]: true});
+        console.log(e.target.name);
+        
+    }
     return (
         <div className='bg-white p-6 absolute top-[68px] z-0 left-0 w-full flex flex-col justify-center items-center text-[#2c3e50] min-h-screen'>
             <div className='container text-center mt-20'>
@@ -13,12 +25,24 @@ export default function Contact() {
             </div>
             <div className="flex justify-center w-full">
                 <form className="bg-white p-6 rounded w-2/3 text-start mb-72">
-                    <input type="text" placeholder='userName' className='mt-1 mb-7 w-full px-3 py-2 border border-transparent border-b-gray-300 rounded-md' />
-                    <input type="text" placeholder='userAge' className='mt-1 mb-7 w-full px-3 py-2 border border-transparent border-b-gray-300 rounded-md' />
-                    <input type="email" placeholder='userEmail' className='mt-1 mb-7 w-full px-3 py-2 border border-transparent border-b-gray-300 rounded-md' />
-                    <input type="password" placeholder='userPassword' className='mt-1 mb-7 w-full px-3 py-2 border border-transparent border-b-gray-300 rounded-md' />
-                    
-                    <button type="submit" className='py-2 px-4 bg-[#1abc9c] text-white font-light rounded-md shadow-sm'>send Message</button>
+                    <div className='relative'>
+                        <input type="text" id='name' name='name' onInput={change} placeholder='userName' className='mt-1 mb-7 w-full px-3 py-2 border border-transparent border-b-gray-300 rounded-md' />
+                        <label htmlFor="name" className={`${style.label} ${isFocus.name ? style.goUp : ''}`}>userName :</label>
+                    </div>
+                    <div className='relative'>
+                        <input type="number" id='age' name='age' onInput={change} placeholder='userAge' className='mt-1 mb-7 w-full px-3 py-2 border border-transparent border-b-gray-300 rounded-md' />
+                        <label htmlFor="age" className={`${style.label} ${isFocus.age ? style.goUp : ''}`}>userAge :</label>
+                    </div>
+                    <div className='relative'>
+                        <input type="email" id='email' name='email' onInput={change} placeholder='userEmail' className='mt-1 mb-7 w-full px-3 py-2 border border-transparent border-b-gray-300 rounded-md' />
+                        <label htmlFor="email" className={`${style.label} ${isFocus.email ? style.goUp : ''}`}>userEmail :</label>
+                    </div>
+                    <div className='relative'>
+                        <input type="password" id='password' name='pass' onInput={change} placeholder='userPassword' className='mt-1 mb-7 w-full px-3 py-2 border border-transparent border-b-gray-300 rounded-md' />
+                        <label htmlFor="password" className={`${style.label} ${isFocus.pass ? style.goUp : ''}`}>userPassword :</label>
+                    </div>                    
+
+                    <button type="submit" onClick={(e) => e.preventDefault()} className='py-2 px-4 bg-[#1abc9c] text-white font-light rounded-md shadow-sm'>send Message</button>
                 </form>
             </div>
         </div>
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -4,14 +4,34 @@ import port2 from '../../assets/images/port2.png'
 import port3 from '../../assets/images/port3.png'
 
 export default function Portfolio() {
-    const [images, setImages] = useState([
-        { src: port1 },
-        { src: port2 },
-        { src: port3 },
-        { src: port1 },
-        { src: port2 },
-        { src: port3 },
-    ]);
+    // const [images, setImages] = useState([
+    //     { src: port1 },
+    //     { src: port2 },
+    //     { src: port3 },
+    //     { src: port1 },
+    //     { src: port2 },
+    //     { src: port3 },
+    // ]);
+    const allImg = [port1, port2, port3, port1, port2, port3]
+    // هونيييييك
+    const [isOpen, setIsOpen] = useState(false);
+    //Handle isOpen
+    function handleIsOpen () {
+        setIsOpen(true);
+    }
+    //Close modal
+    function closeModal () {
+        setIsOpen(false);
+    }
+    const [currentIndex, setCurrentIndex] = useState(1)
+    //Handle Next element
+    function handleNext () {
+        setCurrentIndex((index) => index < allImg.length - 1 ? index + 1 : 0)
+    }
+    //Handle Previous element
+    function handlePrevious () {
+        setCurrentIndex((a) => a > 0 ? a - 1 : allImg.length - 1 /*Or 5*/)
+    }
     return (
         <div className='bg-white p-6 absolute top-[68px] z-0 left-0 w-full flex flex-col justify-center items-center text-[#2c3e50] min-h-screen'>
             <div className='container text-center mt-20'>
@@ -22,14 +42,35 @@ export default function Portfolio() {
                     <div className='w-1/12 border-t-4 border-[#2c3e50]'></div>
                 </div>
             </div>
-            <div className='container grid grid-cols-3 gap-4 p-14 mb-56'>
-                {images.map((images, index) => (
+            <div className='container grid grid-cols-3 gap-4 p-14 mb-56' onClick={handleIsOpen}>
+                {/* {images.map((images, index) => (
                     <div key={index} className='flex items-center justify-center p-3'>
                         <img src={images.src} className=' rounded-lg' />
                     </div>
 
+                ))} */}
+                {allImg.map((ele, index) => (
+                    <div key={index} className='flex items-center justify-center p-3'>
+                        <img src={ele} className=' rounded-lg' />
+                    </div>
+
                 ))}
             </div>
+            {/* هونيييييك */}
+            {isOpen && (
+                <div className='fixed inset-0 bg-slate-400 flex items-center justify-center'>
+                    <div className='bg-red-700 p-3 flex'>
+                        <button onClick={handlePrevious}>⏪</button>
+                        <img src={allImg[currentIndex]} alt="" />
+                        <button onClick={handleNext}>⏩</button>
+                    </div>
+                    <button onClick={closeModal} className='absolute start-3 top-28 rounded-full bg-fuchsia-700'>
+                    x
+                    </button>
+                    
+                </div>
+            )}
+
             
         </div>
     )
